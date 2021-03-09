6e0d7abae5df053a8c6b1719a34027c0c5373f5c
mavros <gavalo.99@gmail.com>
2021-03-09 15:48:22 -0600

    Se crea clase List

---
+++ src/List.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mavros on 9/3/21.
+//
+
+#include "List.h"
---
+++ src/List.h
@@ -0,0 +1,19 @@
+//
+// Created by mavros on 9/3/21.
+//
+
+#ifndef LISTA_DATOS_II_LIST_H
+#define LISTA_DATOS_II_LIST_H
+
+
+#include "Node.h"
+
+class List {
+
+private:
+    Node head;
+
+};
+
+
+#endif //LISTA_DATOS_II_LIST_H
