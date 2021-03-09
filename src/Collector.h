0c57367be104f952b771586b5eaee36c7c9ce560
mavros <gavalo.99@gmail.com>
2021-03-09 16:04:46 -0600

    Se crea clase Collector

---
+++ src/Collector.h
@@ -0,0 +1,19 @@
+//
+// Created by mavros on 9/3/21.
+//
+
+#ifndef LISTA_DATOS_II_COLLECTOR_H
+#define LISTA_DATOS_II_COLLECTOR_H
+
+
+#include "List.h"
+
+class Collector {
+
+private:
+    List recycle_bin;
+
+};
+
+
+#endif //LISTA_DATOS_II_COLLECTOR_H
